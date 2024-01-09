03d8e04fa79d863734788626dfce97e2a15924b9
Jamil Dellawar <j.dellawar5@gmail.com>
2024-01-09 04:58:09 +0000

    Fix autofocusing and take multiple pictures at a time.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from picamera2 import Picamera2, Preview
+from libcamera import controls
 import time
+import os
 
 picam2 = Picamera2(0)
 camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
@@ -8,11 +10,15 @@ picam2.configure(camera_config)
 
 picam2.start_preview(Preview.QTGL)
 picam2.start()
+picam2.set_controls({"AfMode": 1 ,"AfTrigger": 0})
+success = picam2.autofocus_cycle()
 
-time.sleep(2)
 
-picam2.set_controls({"AfMode": 1, "LensPosition": 0})
-
-time.sleep(10)
-
-picam2.capture_file("image.jpg")
+inputY = True
+counter = 0
+while inputY:
+	if input("press 'y' if you want a picture taken") == 'y':
+		picam2.capture_file(f"image{counter}.jpg")
+		counter += 1
+	else:
+		inputY = False
