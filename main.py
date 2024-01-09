b45553a2a4bbc1b95c69a0a6c47fd19a3acded1d
Jamil Dellawar <j.dellawar5@gmail.com>
2024-01-09 03:58:45 +0000

    Take one picture, with test picture

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from picamera2 import Picamera2
-picam2 = Picamera2()
-picam2.start_and_capture_file("image.jpg")
+from picamera2 import Picamera2, Preview
+import time
+
+picam2 = Picamera2(0)
+camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+
+picam2.configure(camera_config)
+
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+
+time.sleep(2)
+
+picam2.capture_file("image.jpg")
